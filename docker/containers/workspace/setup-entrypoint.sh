effbb7699f145a5ae20d94efce3baa8ea834d767
alutskevich <karantin91@gmail.com>
2018-11-30 01:18:15 +0200

    initial commit

---
+++ docker/containers/workspace/setup-entrypoint.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -x
+
+if [ ! -f "/var/www/.env" ];
+then
+    composer create-project && php artisan jwt:secret;
+else
+    composer install;
+fi
+
+php artisan migrate
+
+/bin/bash
